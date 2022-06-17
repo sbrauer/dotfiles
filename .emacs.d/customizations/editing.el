72cb203d3d8fa5a9fd41a52e1e1bd1ffcf45a89a
Sam Brauer <sbrauer@rentpath.com>
2022-06-17 15:47:52 -0400

    Emacs tweaks

--- .emacs.d/customizations/editing.el
+++ .emacs.d/customizations/editing.el
@@ -47,7 +47,20 @@
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
                                                "backups"))))
-(setq auto-save-default nil)
+
+;;(setq auto-save-default nil) ;; disable auto-save
+
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+)
 
 
 ;; comments
@@ -87,3 +100,6 @@
 (global-company-mode)
 (setq company-idle-delay nil) ; never start completions automatically
 (global-set-key (kbd "TAB") #'company-indent-or-complete-common)
+
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
